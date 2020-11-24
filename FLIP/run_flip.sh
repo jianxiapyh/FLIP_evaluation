8621edd7ed7b8ca6c0f507900ef05e8a5d3d9b74
Yihan Pang <yihanp2@illinois.edu>
2020-11-24 04:33:24 -0600

    update for boyuan's need

--- FLIP/run_flip.sh
+++ FLIP/run_flip.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 ref_dir=$1
 test_dir=$2
-ppd=$3
-output_file=$4
+#ppd=$3
+output_file=$3
+offset=$4
 
 clang++ -O3 FLIP.cpp -o Flip
 
-ref_dirlist=$(find $1 -type f | sort -V)
+ref_dirlist=$(find $1 -type f -name "*.png" | sort -V)
 
 for reffile in $ref_dirlist ; do
-        filename="${reffile##*/}"
-        echo "comparing image $filename"
-        testfile="$test_dir$filename"
+        basename="${reffile##*/}"
+#        echo $basename
+        filename="${basename%%.*}"
+        extension="${reffile##*.}"
+ #       echo "comparing image name $filename with extension $extension"
+        mod_filename=$((filename+offset))
+#        echo $mod_filename
+        dot="."
+        testfile="$test_dir$mod_filename$dot$extension"
 #        echo $testfile
-        task="./Flip $reffile $testfile -ppd 11 -outputfile $output_file -v 0 -imagename $filename"
+#        echo $testfile
+        task="./Flip $reffile $testfile -ppd 11 -outputfile $output_file -v 0 -imagename $mod_filename"
         sleep 1
         $task & 
 #        $task 
